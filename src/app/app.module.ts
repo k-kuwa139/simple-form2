6ea50b7ce146fca264c6a4e2e078b4c083fe5c18
k-kuwa139 <kuwa.mogu139@gmail.com>
2019-01-04 16:55:05 +0900

    ~P111

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -8,11 +9,12 @@ import { SimpleFormComponent } from './simple-form/simple-form.component';
 @NgModule({
   declarations: [
     AppComponent,
-    SimpleFormComponent
+    SimpleFormComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/simple-form/simple-form.component.ts
+++ src/app/simple-form/simple-form.component.ts
@@ -6,10 +6,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./simple-form.component.css']
 })
 export class SimpleFormComponent implements OnInit {
-
-  constructor() { }
+  text1:string="0";
+  text2:string="0";
+  result:string= "足し算しましょう";
+  addAndShow() : void {
+    this.result=this.text1;
+    //this=>このクラスから生成されるオブジェクト（Angularではコンポーメント＝オブジェクト）
+  }
+  constructor() {
+    //コンポーメントが作られるときに事項したいプログラムをかく
+  }
 
   ngOnInit() {
+    //コンポーメントが初期化されるときに実行したいプログラムをかく
   }
 
 }
